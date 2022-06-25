17e6a42d2a26d002493cf248d33fb32983d11101
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2022-06-24 21:11:24 -0500

    updated home page to link to the about page after clicking on the homepage btn

--- my-react-portfolio/src/Components/Nav/index.js
+++ my-react-portfolio/src/Components/Nav/index.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { render } from 'react-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import Welcome from '../Welcome';
 import './style.css'
 
 function Nav(pageSelected, setPageSelected) {
+  let time = 0
+  const renders = ()=>{
+    var setTime = setInterval (trackMyScore, 1000);
+    function trackMyScore(){
+      let timer =  document.querySelector('.loader')
+      const score =  timer.textContent = time++ 
+      
+      if(score === 4){
+        return <Welcome />
+      }
+    };
+
+  }
+
   return (
   <>
     <header>
@@ -22,6 +38,12 @@ function Nav(pageSelected, setPageSelected) {
       <div className='logo'>
         <h2 data-text="Jugurta">Jugurta</h2>
       </div>
+
+      <section>
+        <h3 className='loader'>
+        {renders()}
+        </h3>
+      </section>
     </header>
   </>  
 )
